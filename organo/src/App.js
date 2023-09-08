91aa5edf930c3e8dc1ad298b0ba96f077a9b55bd
Ricardo Laurito <ricardolaurito@yahoo.com>
2023-09-08 10:27:12 -0500

    Erro desconhecido

---
+++ organo/src/App.js
@@ -0,0 +1,15 @@
+import ListaSuspensa from './componentes/ListaSuspensa';
+import Banner from './componentes/Banner';
+import Formulario from './componentes/Formulario';
+
+function App() {
+  return (
+    <div className="App">
+      <Banner />
+      <Formulario />
+      <ListaSuspensa />      
+    </div>
+  )
+};
+
+export default App;
